c69797e988ad58d7f5aedfe16a305c8cf776c545
Humma Noshine <Hummanoshine@gmail.com>
2022-05-14 13:44:36 +0100

    start off

---
+++ myportfolio/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import Project from "./components/project";
+
+function App() {
+  return (
+    <div>
+      <Project/>
+    </div>
+  );
+}
+
+export default App;
---
+++ myportfolio/src/components/project.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react';
+import Header from './header';
+import Footer from './footer';
+import Navigation from './navigation';
+
+function ProjectOfMe() {
+
+
+
+    return(
+<div>somehing
+<p>somethinksidhskjdnkusijds </p>
+<div Header></div>
+</div>
+    )
+}
+
+
+export default ProjectOfMe;
