8f04f9fbc4d78e38978a61c1fa0d570fabfd3de4
Humma Noshine <Hummanoshine@gmail.com>
2022-05-19 08:53:49 +0100

    added bootstrap and styling, about me page

--- myportfolio/src/components/aboutme.js
+++ myportfolio/src/components/aboutme.js
@@ -1,23 +1,26 @@
 import React from "react";
+import aboutcss from '../css/about.css'
 
 function AboutME() {
     return(
-        <div id="AboutMe" class="thumbnail">
-            <h1 >About ME</h1>
-            <p id="Iam" >I am an Junior Software developer with a proven track record in Sales and Marketing.</p>
+        <div id="AboutMe" class="aboutthumbnail">
+            <h1 class="me" >About ME</h1>
 
-            <p >I have recently earned a coding certificate in full stack development from the
+            <br></br>
+            <p class="about" >I am an Junior Software developer with a proven track record in Sales and Marketing.</p>
+
+            <p class="about" >I have recently earned a coding certificate in full stack development from the
               University of Birmingham. I have newly developed skills in HTML, JavaScript, CSS, React.js, responsive web design and a lot more!!</p>
               
-              <p >I am interested in managing software operations, problem-solving, designing user-face products and coding. I am looking to enhance my career by proving the knowledge and skills I have recently received. 
+              <p class="about" >I am interested in managing software operations, problem-solving, designing user-face products and coding. I am looking to enhance my career by proving the knowledge and skills I have recently received. 
               My biggest strength and also what I am most known for is,  to be an innovative problem solver and passionate about ‘cracking code’ whilst developing apps which are focused around mobile designs and development. 
               With every project I work on, my intention is to always ensure my audience are engaged for an impactful user experience. </p>
               
-              <p> I love working in a collaborative environment, I have worked on a team of 5-6 members whom each work together in order to create quality projects. </p>
+              <p class="about"> I love working in a collaborative environment, I have worked on a team of 5-6 members whom each work together in order to create quality projects. </p>
               
-              <p > I am very excited to leverage my skills as part of a fast-paced, quality-driven team to build better experiences on the web.
+              <p class="about"> I am very excited to leverage my skills as part of a fast-paced, quality-driven team to build better experiences on the web.
               </p>
-           <img id="funnyPic"src="./css/assets/wp1828911-programmer-wallpapers.jpg" alt="funny coders background"/>
+           <img id="funnyPic"src="../assets/pepsi.jpg" alt="funny coders background"/>
            
           </div>
     )
