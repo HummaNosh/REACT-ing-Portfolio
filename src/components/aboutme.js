36fc03a5e2e7060f404ac20bc3cd9957464d5690
Humma Noshine <Hummanoshine@gmail.com>
2022-05-21 12:36:47 +0100

    removed main folder, fixed about me

--- src/components/aboutme.js
+++ src/components/aboutme.js
@@ -14,8 +14,8 @@ function AboutME() {
             <li class="text about" >I am an Junior Software developer with a proven track record in Sales and Marketing. 
              I have recently earned a coding certificate in full stack development from the University of Birmingham. I have newly developed skills in HTML, JavaScript, CSS, React.js, responsive web design and a lot more!!I am interested in managing software operations, problem-solving, designing user-face products and coding. 
              I am looking to enhance my career by proving the knowledge and skills I have recently received. </li>
-             <li class="text about"> My biggest strength and also what I am most known for is,  to be an innovative problem solver and passionate about ‘cracking code’ whilst developing apps which are focused around mobile designs and development.  With every project I work on, my intention is to always ensure my audience are engaged for an impactful user experience.
-            I love working in a collaborative environment, I have worked on a team of 5-6 members whom each work together in order to create quality projects. I am very excited to leverage my skills as part of a fast-paced, quality-driven team to build better experiences on the web.</li> 
+             <li class="text about"> My biggest strength and also what I am most known for is,  to be an innovative problem solver and passionate about ‘cracking code’ whilst developing apps which are focused around mobile designs and development.  With every project I work on, my intention is to always ensure my audience are engaged for an impactful user experience.</li>
+            <li class="text about">I love working in a collaborative environment, I have worked on a team of 5-6 members whom each work together in order to create quality projects. I am very excited to leverage my skills as part of a fast-paced, quality-driven team to build better experiences on the web.</li> 
   
         <img class= "pic" src={me} alt="An avatar of Humma"/>
         </article>
